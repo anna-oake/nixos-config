b3b5137fed2b09972abd22d9ef7fcb5ca539f1db
Anna Oake <anna@oa.ke>
2025-07-03 13:01:21 +0200

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,10 @@
 let
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHf6UCNeXSN8WAZ9cXh8jz61+jbP+ts+inct/CCjcN/o";
   june = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPFUV/VR3syeQplSZcUiBbe1QG4E0C36kWzlhJYMbWEQ";
+  ara-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBaXGEBuUUsviWKNUGc4qnSxMCN0jfausbqZ7whFr2jS";
   hosts = [
     june
+    ara-nixos
   ];
   all = [ user ] ++ hosts;
 in
