673566f66d8ff2d07b57241bee5fa54f9cd4537b
Anna Oake <anna@oa.ke>
2025-07-03 12:50:41 +0200

    more ara

--- hosts/nixos/ara/hardware-configuration.nix
+++ hosts/nixos/ara/hardware-configuration.nix
@@ -5,15 +5,18 @@
 {
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
-  boot.initrd.availableKernelModules = [ "usb_storage" ];
+  boot.initrd.availableKernelModules = [
+    "usb_storage"
+    "sdhci_pci"
+  ];
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/0337c57f-cdf1-40b6-a9fd-ff5077ab2fd2";
+      device = "/dev/disk/by-uuid/0c0525cb-6dda-41f4-9208-21c3bbaa8656";
       fsType = "ext4";
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/1466-1BF0";
+      device = "/dev/disk/by-uuid/3242-1D16";
       fsType = "vfat";
       options = [
         "fmask=0022"
