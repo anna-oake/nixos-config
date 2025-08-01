0f818dd3dd74224313f326da5016990f602eebdb
Anna Oake <anna@oa.ke>
2025-08-01 16:15:25 +0200

    eule: switch to grub

--- hosts/eule/configuration.nix
+++ hosts/eule/configuration.nix
@@ -12,7 +12,9 @@
     flake.nixosModules.default
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.useOSProber = true;
+
   boot.loader.efi.canTouchEfiVariables = true;
 
   environment.systemPackages = with pkgs; [
