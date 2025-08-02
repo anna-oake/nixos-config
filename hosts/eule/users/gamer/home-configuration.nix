8af9b7f89ecf7cd3dd72ccf00d316748c35c6e6a
Anna Oake <anna@oa.ke>
2025-08-02 19:57:24 +0200

    try that

--- hosts/eule/users/gamer/home-configuration.nix
+++ hosts/eule/users/gamer/home-configuration.nix
@@ -1,5 +1,21 @@
-{ pkgs, osConfig, ... }:
 {
+  pkgs,
+  osConfig,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.plasma-manager.homeManagerModules.plasma-manager
+  ];
+
+  programs.plasma = {
+    enable = true;
+    workspace = {
+      theme = "breeze-dark";
+      colorScheme = "BreezeDark";
+    };
+  };
 
   home.stateVersion = "25.11";
 }
