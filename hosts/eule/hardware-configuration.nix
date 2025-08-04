542dbf13418b5f0bdf3944800d948fd6b5b0c71e
Anna Oake <anna@oa.ke>
2025-08-04 18:46:31 +0200

    Update hardware-configuration.nix

--- hosts/eule/hardware-configuration.nix
+++ hosts/eule/hardware-configuration.nix
@@ -34,18 +34,11 @@ in
     pkgs.rtl8761bu-firmware
   ];
 
-  boot.kernelParams = [
-    # "btusb.enable_autosuspend=0"
-    # "pcie_aspm=off"
-    # "usbcore.autosuspend=-1"
-  ];
-
   services.udev.extraRules = ''
     # Allow this Realtek BT dongle to wake the system
-    # ACTION=="add|change", SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="a729", \
-    #   ATTR{power/wakeup}="enabled", ATTR{power/control}="on"
-
-    # SUBSYSTEM=="pci", DRIVER=="xhci_hcd", ATTR{power/wakeup}="enabled", ATTR{power/control}="on"
+    ACTION=="add|change", SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="a729", \
+      ATTR{power/wakeup}="enabled"
+    # , ATTR{power/control}="on"
   '';
 
   fileSystems = {
