a678731be54e587eea80d8996ac7e1b9ba6bf889
Anna Oake <anna@oa.ke>
2025-08-30 14:41:28 +0200

    ghostty <3

--- modules/lxc/user.nix
+++ modules/lxc/user.nix
@@ -5,9 +5,15 @@
 }:
 {
   users.mutableUsers = false;
+
   users.users.root.openssh.authorizedKeys.keys = [
     config.me.sshKey
   ];
+
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  environment.systemPackages = with pkgs; [
+    ghostty.terminfo
+  ];
 }
--- modules/nixos/stuff.nix
+++ modules/nixos/stuff.nix
@@ -31,5 +31,6 @@
     usbutils
     pciutils
     go
+    ghostty.terminfo
   ];
 }
