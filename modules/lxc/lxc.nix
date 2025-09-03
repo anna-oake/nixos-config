38af3789e2c55104373025652ff905dc4f20aac4
Anna Oake <anna@oa.ke>
2025-09-03 22:49:04 +0200

    lxc: factor out into nix-things

--- modules/lxc/lxc.nix
+++ modules/lxc/lxc.nix
@@ -1,27 +1,13 @@
 {
   inputs,
-  modulesPath,
   lib,
   ...
 }:
 {
   imports = [
-    "${modulesPath}/virtualisation/proxmox-lxc.nix"
     inputs.self.commonModules.default
-    inputs.nix-things.nixosModules.default
-    ./profiles
+    inputs.nix-things.lxcModules.default
   ];
-
-  systemd.suppressedSystemUnits = [
-    "sys-kernel-debug.mount"
-  ];
-
-  proxmoxLXC.manageNetwork = true;
-  networking.firewall.enable = false;
-
-  age.identityPaths = [ "/nix-lxc/agenix_key" ];
 
   lxc.pve.host = lib.mkDefault "mynah.lan.ci";
-
-  nixpkgs.hostPlatform = "x86_64-linux";
 }
