93b2dcfa828910ba78c29320f19bfbbaf9cb6953
Anna Oake <anna@oa.ke>
2025-09-03 16:12:51 +0200

    lxc: make pve.host default

--- modules/lxc/lxc.nix
+++ modules/lxc/lxc.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   modulesPath,
+  lib,
   ...
 }:
 {
@@ -20,7 +21,7 @@
 
   age.identityPaths = [ "/nix-lxc/agenix_key" ];
 
-  lxc.pve.host = "mynah.lan.ci";
+  lxc.pve.host = lib.mkDefault "mynah.lan.ci";
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
