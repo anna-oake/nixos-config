5eadbf3534fde79cb824eddfb88f72f15b126914
Anna Oake <anna@oa.ke>
2025-09-03 16:23:31 +0200

    lxc-net-router (#19)

--- modules/lxc/lxc.nix
+++ modules/lxc/lxc.nix
@@ -1,12 +1,14 @@
 {
   inputs,
   modulesPath,
+  lib,
   ...
 }:
 {
   imports = [
     "${modulesPath}/virtualisation/proxmox-lxc.nix"
     inputs.self.commonModules.default
+    inputs.nix-things.nixosModules.default
     ./profiles
   ];
 
@@ -19,7 +21,7 @@
 
   age.identityPaths = [ "/nix-lxc/agenix_key" ];
 
-  lxc.pve.host = "mynah.lan.ci";
+  lxc.pve.host = lib.mkDefault "mynah.lan.ci";
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
