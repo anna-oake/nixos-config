e61b8f81af73e6342952798242c679869cda8321
Anna Oake <anna@oa.ke>
2025-09-04 11:31:06 +0200

    fix deploy-rs

--- flake.nix
+++ flake.nix
@@ -72,11 +72,6 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
