35b439aebec2129754f3b5b98e8eefec34935c40
Anna Oake <anna@oa.ke>
2026-01-03 15:19:33 +0100

    lxc-monitor: init

---
+++ hosts/lxc-monitor/configuration.nix
@@ -0,0 +1,20 @@
+{
+  inputs,
+  config,
+  ...
+}:
+{
+  imports = [
+    inputs.self.nixosModules.default
+  ];
+
+  profiles.server.enable = true;
+
+  lxc = {
+    enable = true;
+    network = "vmbr1"; # important! this is deployed to a router where vmbr0 is WAN
+    pve.host = "kolibri." + config.me.lanDomain;
+  };
+
+  system.stateVersion = "25.11";
+}
