2fb04fc81d5ce6eb829ade9ff3ce82b6a6c2b139
Anna Oake <anna@oa.ke>
2026-01-04 15:23:16 +0100

    lxc-monitor: add graylog

--- hosts/lxc-monitor/configuration.nix
+++ hosts/lxc-monitor/configuration.nix
@@ -8,10 +8,21 @@
     inputs.self.nixosModules.default
   ];
 
-  profiles.server.enable = true;
+  profiles.server.monitor.enable = true;
+
+  profiles.server.net-router = {
+    enable = true;
+    port = 30305;
+    enableForwarding = false;
+    tokenType = "monitor";
+  };
 
   lxc = {
     enable = true;
+    memory = 8192;
+    mounts = [
+      "lxc:monitor-storage,mp=/storage,backup=1,size=100G"
+    ];
     network = "vmbr1"; # important! this is deployed to a router where vmbr0 is WAN
     pve.host = "kolibri." + config.me.lanDomain;
   };
