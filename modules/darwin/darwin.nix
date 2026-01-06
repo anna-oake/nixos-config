5aec90d33595f42bbbcef1a94c4e1c8231d14b4e
Anna Oake <anna@oa.ke>
2026-01-06 22:33:30 +0100

    darwin: add builders for {x86_64,aarch64}-linux

--- modules/darwin/darwin.nix
+++ modules/darwin/darwin.nix
@@ -50,4 +50,21 @@
 
     taps = builtins.attrNames config.nix-homebrew.taps;
   };
+
+  launchd.daemons.nix-daemon.environment = {
+    SSH_AUTH_SOCK = "/Users/anna/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  };
+  remoteBuilders = {
+    protocol = "ssh";
+    machines = {
+      gratis = {
+        enable = true;
+        sshUser = "root";
+      };
+      lxc-builder-kitezh = {
+        enable = true;
+        sshUser = "root";
+      };
+    };
+  };
 }
