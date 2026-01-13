59c6d412be05fd79c99f5ba7f2d31e3bc532d27d
Anna Oake <anna@oa.ke>
2026-01-13 21:39:12 +0100

    secrets: make agenix-rekey chill out about 'leaked' keys

--- modules/common/agenix.nix
+++ modules/common/agenix.nix
@@ -11,8 +11,10 @@
   config = {
     age.rekey.masterIdentities = [
       {
-        identity = inputs.self.outPath + "/secrets/master-keys/agenix-master-pq.key";
-        pubkey = builtins.readFile (inputs.self.outPath + "/secrets/master-keys/agenix-master-pq.pub");
+        identity = inputs.self.outPath + "/secrets/master-keys/agenix-master-pq-identity.pub";
+        pubkey = builtins.readFile (
+          inputs.self.outPath + "/secrets/master-keys/agenix-master-pq-recipient.pub"
+        );
       }
     ];
   };
