f361847b17cb4070af85b46e1d8e321cfa4e477d
Anna Oake <anna@oa.ke>
2026-01-21 16:09:31 +0100

    lxc-docker: init (#67)

---
+++ hosts/lxc-docker/configuration.nix
@@ -0,0 +1,35 @@
+{
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.self.nixosModules.default
+  ];
+
+  profiles.server.docker = {
+    enable = true;
+    enableNvidiaSupport = true;
+
+    portainer = {
+      enable = true;
+      traefikIntegration.host = "ph.koteeq.me";
+    };
+  };
+
+  lxc = {
+    enable = true;
+    cores = 16;
+    memory = 30720;
+    diskSize = 80;
+    mounts = [
+      "/storage,mp=/storage"
+      "/storage-fast,mp=/storage-fast"
+      "/storage-fast/attic,mp=/storage-fast/attic"
+      "/storage-fast/docker,mp=/var/lib/docker/volumes"
+    ];
+    nvidia.enable = true;
+  };
+
+  system.stateVersion = "25.11";
+}
