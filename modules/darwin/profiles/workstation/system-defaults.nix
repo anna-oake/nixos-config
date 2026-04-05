f3e213ff1c8c9144a651f06e5c5bddd9ab54a385
Anna Oake <anna@oa.ke>
2026-04-05 14:37:47 +0200

    darwin: use profiles

---
+++ modules/darwin/profiles/workstation/system-defaults.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  config = lib.mkIf config.profiles.workstation.enable {
+    system.defaults = {
+      dock = {
+        autohide = true;
+        autohide-delay = 0.0;
+        autohide-time-modifier = 0.3;
+        tilesize = 70;
+        orientation = "bottom";
+        magnification = false;
+        show-recents = false;
+        # top left hot corner - start screen saver
+        wvous-tl-corner = 5;
+        # bottom right hot corner - do nothing
+        wvous-br-corner = 1;
+        persistent-apps = [
+          "/System/Cryptexes/App/System/Applications/Safari.app"
+          "/System/Applications/Calendar.app"
+          "/System/Applications/Mail.app"
+          "/Applications/Nix Apps/Spotify.app"
+          "/Applications/Telegram.app"
+          "/Applications/Nix Apps/Slack.app"
+          "/Applications/Ghostty.app"
+          "${pkgs.zed-editor}/Applications/Zed.app"
+          "/Applications/Nix Apps/ChatGPT.app"
+          "/Applications/Modrinth App.app"
+        ];
+      };
+    };
+  };
+}
--- modules/darwin/system-defaults.nix
+++ modules/darwin/system-defaults.nix
@@ -154,32 +154,6 @@
       QuitMenuItem = true;
     };
 
-    dock = {
-      autohide = true;
-      autohide-delay = 0.0;
-      autohide-time-modifier = 0.3;
-      tilesize = 70;
-      orientation = "bottom";
-      magnification = false;
-      show-recents = false;
-      # top left hot corner - start screen saver
-      wvous-tl-corner = 5;
-      # bottom right hot corner - do nothing
-      wvous-br-corner = 1;
-      persistent-apps = [
-        "/System/Cryptexes/App/System/Applications/Safari.app"
-        "/System/Applications/Calendar.app"
-        "/System/Applications/Mail.app"
-        "/Applications/Nix Apps/Spotify.app"
-        "/Applications/Telegram.app"
-        "/Applications/Nix Apps/Slack.app"
-        "/Applications/Ghostty.app"
-        "${pkgs.zed-editor}/Applications/Zed.app"
-        "/Applications/Nix Apps/ChatGPT.app"
-        "/Applications/Modrinth App.app"
-      ];
-    };
-
     loginwindow = {
       LoginwindowText = "Please contact ${config.me.email}";
     };
