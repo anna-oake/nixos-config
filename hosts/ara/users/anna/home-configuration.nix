c1bd0011c62df3b2d9b60465167502d064ad0ba0
Anna Oake <anna@oa.ke>
2026-04-05 14:37:25 +0200

    ara: add docker

--- hosts/ara/users/anna/home-configuration.nix
+++ hosts/ara/users/anna/home-configuration.nix
@@ -1,4 +1,6 @@
 {
+  pkgs,
+  config,
   inputs,
   ...
 }:
@@ -8,6 +10,53 @@
   ];
 
   profiles.workstation.enable = true;
+
+  home.packages = with pkgs; [
+    docker
+    docker-compose
+    docker-buildx
+    docker-credential-helpers
+  ];
+
+  programs.docker-cli = {
+    enable = true;
+    settings = {
+      credsStore = "osxkeychain";
+    };
+  };
+
+  home.file."${config.programs.docker-cli.configDir}/cli-plugins/docker-buildx".source =
+    "${pkgs.docker-buildx}/bin/docker-buildx";
+  home.file."${config.programs.docker-cli.configDir}/cli-plugins/docker-compose".source =
+    "${pkgs.docker-compose}/bin/docker-compose";
+
+  programs.lazydocker.enable = true;
+
+  services.colima = {
+    enable = true;
+
+    profiles.default = {
+      isService = true;
+      isActive = true;
+
+      settings = {
+        runtime = "docker";
+        arch = "host";
+
+        vmType = "vz";
+        mountType = "virtiofs";
+        mounts = [ ];
+
+        cpu = 4;
+        memory = 8;
+        disk = 100;
+
+        kubernetes.enabled = false;
+
+        portForwarder = "ssh";
+      };
+    };
+  };
 
   programs.hammerspoon = {
     enable = true;
