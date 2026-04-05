c8053872821936e3eed8a2043d1e2b487439f30b
Anna Oake <anna@oa.ke>
2026-04-05 18:51:40 +0200

    me: update sshKey

--- modules/common/me.nix
+++ modules/common/me.nix
@@ -2,7 +2,7 @@
   config.me = {
     username = "anna";
     email = "anna@oa.ke";
-    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHf6UCNeXSN8WAZ9cXh8jz61+jbP+ts+inct/CCjcN/o anna@oa.ke";
+    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2jBAbYXwrY13EBAVyk5QaG3xOR0d9IfbpkgP8/anna";
     wifeKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZNKMNZpuqfniJkCK+//Cz+hI3WH2l78Txp9I/maeve";
     deployKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILoOFVtemQpoM9Vj7bPPuwpdcH7nYwEINq9gokMAJX/m";
     lanDomain = "lan.al";
