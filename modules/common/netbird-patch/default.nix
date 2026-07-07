33d43b00121dc732d3a737d1afe1facb66a8ddbb
Anna Oake <anna@oa.ke>
2026-07-07 23:25:09 +0200

    darwin: deprioritise netbird peer domain

--- modules/common/netbird-patch/default.nix
+++ modules/common/netbird-patch/default.nix
@@ -5,7 +5,7 @@
 {
   services.netbird.package = pkgs.netbird.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [
-      ./lazy-routing-fix.patch
+      ./darwin-peer-domain-no-search.patch
     ];
   });
 }
