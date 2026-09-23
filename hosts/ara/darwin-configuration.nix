f54305633f9bbb6845f294faaa235f0bf4189856
Anna Oake <anna@oa.ke>
2026-09-23 16:04:57 +0200

    ara: enable linux-builder

--- hosts/ara/darwin-configuration.nix
+++ hosts/ara/darwin-configuration.nix
@@ -12,6 +12,29 @@
 
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  nix.linux-builder = {
+    enable = true;
+
+    systems = [
+      "aarch64-linux"
+    ];
+
+    speedFactor = 10;
+
+    ephemeral = false;
+
+    config = {
+      virtualisation = {
+        cores = 10;
+
+        darwin-builder = {
+          memorySize = 12 * 1024;
+          diskSize = 80 * 1024;
+        };
+      };
+    };
+  };
+
   # Do not remove
   system.stateVersion = 5;
 }
